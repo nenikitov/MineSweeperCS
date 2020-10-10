6d4b4b98d4445db076570f8dc10ab6faa4e43e6e
unknown <nenikitov@mail.ru>
2020-10-09 22:28:42 -0400

    Changed default difficulty

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace NewMineSweeper
     class Program
     {
         static int[] fieldSize = { 10, 5 };
-        static double difficulty = 0.15;
+        static double difficulty = 0.2;
         enum DisplayTileStatus
         {
             Unopened,
